568ecc4af0bc0f373d3366f8cf17c4c6de9380b9
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-25 16:47:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,11 +12,10 @@ class CashRegister
   end
 
   def apply_discount
-    @total = @total - @total * @discount/100
-    "After the discount, the total comes to $#{@total}."
     if @discount == 0
       "There is no discount to apply."
+    else @total = @total - @total * @discount/100
+    "After the discount, the total comes to $#{@total}."
     end
-
   end
 end
